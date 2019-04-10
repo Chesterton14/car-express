0db292af6a608c5e813ff5bf11942e6664597a23
xuzuhao <1326288580@qq.com>
2019-04-10 20:06:28 +0800

    Update mobile.js

--- routes/mobile.js
+++ routes/mobile.js
@@ -53,7 +53,8 @@ router.post('/new',function (req,res) {
 router.post('/data',function (req,res) {
     const data = req.body;
     const sql='INSERT INTO points(lat,lng,carId,time) VALUES(?,?,?,?)';
-    connection.query(sql,[data.lat,data.lng,data.carId,data.time],function (err,result) {
+    let time = new Date().format("yyyy-MM-dd hh:mm:ss");
+    connection.query(sql,[data.lat,data.lng,data.carId,time],function (err,result) {
         if (err){
             console.log("[INSERT ERR]",err);
             res.send({
@@ -71,3 +72,24 @@ router.post('/data',function (req,res) {
 
 
 module.exports = router;
+
+Date.prototype.format = function (fmt) {
+    let o = {
+        "M+": this.getMonth() + 1,                 //月份
+        "d+": this.getDate(),                    //日
+        "h+": this.getHours(),                   //小时
+        "m+": this.getMinutes(),                 //分
+        "s+": this.getSeconds(),                 //秒
+        "q+": Math.floor((this.getMonth() + 3) / 3), //季度
+        "S": this.getMilliseconds()             //毫秒
+    };
+    if (/(y+)/.test(fmt)) {
+        fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    }
+    for (let k in o) {
+        if (new RegExp("(" + k + ")").test(fmt)) {
+            fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+        }
+    }
+    return fmt;
+};
