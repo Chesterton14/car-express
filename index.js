d0f7c11f8c17a38639faeceae57b5229b453c690
xuzuhao <1326288580@qq.com>
2019-06-26 16:40:17 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,16 +2,17 @@
 var http = require('http')
 var spawn = require('child_process').spawn
 var createHandler = require('github-webhook-handler')
-var handler = createHandler({ path: '/pushCode', secret: 'demo' }) // 在代码仓库的 Webhooks 选项处配置
+var handler = createHandler({ path: '/', secret: 'demo' }) // 在代码仓库的 Webhooks 选项处配置
 http.createServer(function (req, res) {
     handler(req, res, function (err) {
+        if  (err) console.log(err)
         res.statusCode = 404;
         res.end('no such location')
     })
 }).listen(7777)
 
 handler.on('error', function (err) {
-    console.error('Error:', err.message)
+    console.log('Error:', err.message)
 })
 
 // 监听 push 事件
