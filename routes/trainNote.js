64b72cf42337370047be6345ce0dfb66a8764cb8
xuzuhao <1326288580@qq.com>
2019-09-19 17:10:44 +0800

    新增拉取动作api

--- routes/trainNote.js
+++ routes/trainNote.js
@@ -16,4 +16,28 @@ router.get('/part',function(req,res){
     });
 })
 
+router.get('/action',function(req,res){
+    const partId = req.query.partId.replace(/[\'\"\\\/\b\f\n\r\t]/g, '');
+    const sql = "SELECT * FROM trainNote_action WHERE partId=" + partId;
+    connection(sql, function (err, result) {
+        if (err) {
+            console.log('[SELECT ERROR] - ', err.message);
+            res.send(404);
+            return;
+        }
+        if (result == '') {
+            res.send({
+                status: 500,
+                msg: '无此车辆数据！'
+            })
+        } else {
+            res.send({
+                status: 200,
+                msg: '查找车辆成功！',
+                data: result
+            })
+        }
+    })
+})
+
 module.exports = router;
